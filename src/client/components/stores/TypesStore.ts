ec7ac0f5eace6ecb49f77930c502b6a8f614c8c0
maxim-puzikov <swsw32254@gmail.com>
2018-03-16 13:36:56 +0200

    reformat TypesStore

--- src/client/components/stores/TypesStore.ts
+++ src/client/components/stores/TypesStore.ts
@@ -1,27 +1,27 @@
-import { ReduceStore } from "flux/utils";
+import {ReduceStore} from "flux/utils";
 import TypeDispatcher from "../../dispatcher/TypeDispatcher";
-import { TypeActionTypes } from "../../constants/TypeActionTypes";
+import {TypeActionTypes} from "../../constants/TypeActionTypes";
 import {Type} from "../../models/Type";
 
 
 class TypesStore extends ReduceStore<Type[], any> {
-    constructor() {
-	    super(TypeDispatcher);
-    }
+	constructor() {
+		super(TypeDispatcher);
+	}
 
 	getInitialState(): Type[] {
 		return [];
 	}
 
-    reduce(state, action): Type[] {
-        switch (action.eventName) {
-            case TypeActionTypes.ADD_TYPE:
-		        return [...state, action. type];
+	reduce(state, action): Type[] {
+		switch (action.eventName) {
+			case TypeActionTypes.ADD_TYPE:
+				return [...state, action.type];
 
-            default:
-                return state;
-        }
-    }
+			default:
+				return state;
+		}
+	}
 }
 
 export default new TypesStore();
